a2f7493a93e2a577b0aa9d46637380f0fd81b5dc
Breno Prado <breno.o.prado@hotmail.com>
2022-07-01 17:19:34 -0300

    Requisito 2 :turtle:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import Search from './pages/Search';
+import AllPages from './pages/AllPages';
 
 class App extends React.Component {
   render() {
@@ -16,15 +9,7 @@ class App extends React.Component {
           <h3>TrybeTunes</h3>
           <nav>area de navegação</nav>
         </header>
-        <Switch>
-          <Route path="/" exact component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" exact component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
+        <AllPages />
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,95 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../component/Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      lessThanThree: true,
+      loading: false,
+    };
+  }
+
+  inputChange = ({ target }) => {
+    const minNun = 3;
+    this.setState({
+      inputValue: target.value,
+    }, () => {
+      if (target.value.length >= minNun) {
+        this.setState({
+          lessThanThree: false,
+        });
+      } else {
+        this.setState({
+          lessThanThree: true,
+        });
+      }
+    });
+  }
+
+  clickButton = () => {
+    const { inputValue } = this.state;
+    const { history } = this.props;
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name: inputValue });
+      history.push('/search');
+    });
+  }
+
   render() {
+    const {
+      inputValue,
+      lessThanThree,
+      loading,
+    } = this.state;
+    if (loading) return (<Loading />);
     return (
-      <div data-testid="page-login">
-        <h1>Login</h1>
+      <div data-testid="page-login" className="login_page">
+        <h4>Login</h4>
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            onChange={ (event) => this.inputChange(event) }
+            value={ inputValue }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            onClick={ () => this.clickButton() }
+            disabled={ lessThanThree }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propsTypes = {
+  history: PropTypes.shape({
+    action: PropTypes.string,
+    block: PropTypes.func,
+    createHref: PropTypes.func,
+    go: PropTypes.func,
+    goBack: PropTypes.func,
+    goForward: PropTypes.func,
+    length: PropTypes.number,
+    listen: PropTypes.func,
+    location: PropTypes.shape({
+      hash: PropTypes.string,
+      pathname: PropTypes.string,
+      state: undefined,
+    }),
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func,
+  }),
+};
 
 export default Login;
