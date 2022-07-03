b112d0d648f7205b797fb6db3937377e034ad212
Breno Prado <breno.o.prado@hotmail.com>
2022-07-03 12:38:26 -0300

    Requisito 5 :turtle:

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      disabledButton: true,
+    };
+  }
+
+  inputChange = ({ target }) => {
+    this.setState({
+      inputValue: target.value,
+    }, () => {
+      if (target.value.length >= 2) {
+        this.setState({
+          disabledButton: false,
+        });
+      }
+    });
+  }
+
   render() {
+    const { inputValue, disabledButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <form>
+          <label htmlFor="artist-input">
+            Pesquisa:
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="artist-input"
+              value={ inputValue }
+              onChange={ (event) => this.inputChange(event) }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              name="artist-button"
+              disabled={ disabledButton }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
